ef8fb87eb9615c92d5234a741e0472a85cec0a3d
Richard Chan <r32095@gmail.com>
2025-05-10 09:44:22 -0500

    fix stories

--- src/app/pages/rename.tsx
+++ src/app/pages/rename.tsx
@@ -5,7 +5,7 @@ import { get } from "../lib/api";
 import { useRename } from "./hooks/useRename";
 import { RenameProvider } from "./provider/RenameProvider";
 import PreviewFiles from "./stages/forms/PreviewFiles";
-import RenameUploadStage from "./stages/Upload";
+import RenameUploadStage from "./stages/RenameUpload";
 import SetStreams from "./stages/SetStreams";
 import NameChangePreview from "./stages/NameChange";
 
--- src/app/pages/stages/setStreams.stories.tsx
+++ src/app/pages/stages/setStreams.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import SetStreams from "./SetStreams";
+import { RenameProvider } from "../provider/RenameProvider";
 
 const meta = {
     component: SetStreams,
     title: "Set Stream Stage",
     tags: ["autodocs"],
+    render: (args) => {
+        return (
+            <RenameProvider>
+                <SetStreams {...args} />
+            </RenameProvider>
+        )
+    },
 } satisfies Meta<typeof SetStreams>;
 
 export default meta;
