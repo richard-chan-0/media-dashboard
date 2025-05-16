b4f6d4884daf5911b00c1314eef45ce66b034942
Richard Chan <r32095@gmail.com>
2025-05-16 15:59:45 -0500

    refactor upload stage button based on upload files or existing files

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
     globalIgnores([
