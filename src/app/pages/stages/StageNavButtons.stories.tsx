9ee9e9263710bbf44b647f4ba01a77a821ab183f
Richard Chan <r32095@gmail.com>
2025-05-18 23:18:49 -0500

    rename files in git

--- src/app/pages/stages/StageNavButtons.stories.tsx
+++ src/app/pages/stages/StageNavButtons.stories.tsx
@@ -12,21 +12,18 @@ type Story = StoryObj<typeof meta>;
 
 export const LeftButtonWithLabel: Story = {
     args: {
-        stageDispatcher: () => { },
         isLeftEnabled: true,
     },
 };
 
 export const RightButtonWithLabel: Story = {
     args: {
-        stageDispatcher: () => { },
         isRightEnabled: true,
     },
 };
 
 export const BothButtonsWithLabel: Story = {
     args: {
-        stageDispatcher: () => { },
         isLeftEnabled: true,
         isRightEnabled: true,
     },
