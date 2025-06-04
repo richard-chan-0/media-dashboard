ff7c1d1b9d7ec4c9544c919be479bae34e68ca4b
Richard Chan <r32095@gmail.com>
2025-06-04 04:10:27 -0500

    add first tests and resolve vitest and storybook issue

--- src/app/pages/stages/index.ts
+++ src/app/pages/stages/index.ts
@@ -1,3 +1,3 @@
-export {default as NameChangePreview} from "./nameChange";
-export {default as SetStreams} from "./setStreams";
-export {default as RenameUploadStage} from "./upload";
+export {default as NameChangePreview} from "./NameChange";
+export {default as SetStreams} from "./SetStreams";
+export {default as RenameUploadStage} from "./RenameUpload";
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+    },
 });
